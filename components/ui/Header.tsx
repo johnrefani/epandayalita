08cf995de78fdf1570a4b32a369f419c01e1407f
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-03-28 15:29:02 +0800

    Configure Merriweather font and custom theme colors

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='flex-center h-20'>
+      <h1 className='text-4xl'>Header</h1>
+    </header>
   )
 }
 
