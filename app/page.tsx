44d90bc2e6ad49b2e3ae45db6649d2e8d60242f8
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-03-30 14:14:38 +0800

    apply margin to all sections

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,12 @@ from "@/lib/imports";
 
 export default function Home() {
   return (
-    <main className="mx-[1rem] md:mx-[3.75rem] lg:mx-[6.25rem] xl:mx-[7.5rem]">
+    <main>
       <Header/> 
+      <div className="mx-[1rem] md:mx-[3.75rem] lg:mx-[6.25rem] xl:mx-[7.5rem]">
       <Main/>
       <About/>
+      </div>
     </main>
   );
 }
