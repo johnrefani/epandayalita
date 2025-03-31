9b189e29ec20026aad05f46443dd14509b265785
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-03-31 22:51:37 +0800

    add rescard and catcard

--- components/ui/ResCard.tsx
+++ components/ui/ResCard.tsx
@@ -1,41 +1,39 @@
+import { ResCardProps } from "@/data/props";
 import Image from "next/image";
 import React from "react";
 import { HiSpeakerWave } from "react-icons/hi2";
 
-const ResCard = () => {
+
+const ResCard = ({ title, pic, speech, phonetic, description } : ResCardProps) => {
   return (
     <section className="relative flex-center pt-16 mt-20">
-     
       <div className="absolute inset-0 grid grid-rows-[60%_40%]">
         <div className="bg-green-700"></div>
         <div className="bg-white"></div>
       </div>
 
       <div className="relative z-10 w-xl md:w-2xl lg:w-3xl xl:w-5xl flex px-16 py-14 rounded-md shadow-lg bg-white mb-10">
-        <Image
-          className="border border-green-900"
-          src="/favicon.svg"
-          width={250}
-          height={250}
-          alt="Hammer"
-        />
+        <div className="min-h-[250] max-w-[300px]">
+          <Image
+            className="border border-green-900 object-cover"
+            src={pic}
+            width={300}
+            height={300}
+            alt={title}
+          />
+        </div>
 
         <div className="py-2 px-4 flex flex-col">
           <div className="gap-6 flex">
-            <h1 className="text-3xl font-semibold">Hammer</h1>
+            <h1 className="text-3xl font-semibold">{title}</h1>
             <HiSpeakerWave size={30} className="self-center" />
           </div>
           <h2 className="w-fit mt-2 rounded-md border shadow py-2 px-4 text-base">
-            /mar-tee-lyo/
+            {phonetic}
           </h2>
           <div className="mt-8">
-            <h2 className="text-xl font-bold">verb (Pandiwa)</h2>
-            <p className="text-sm opacity-75 mt-4">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Expedita perferendis consequuntur rerum molestiae, quia error,
-              distinctio illum blanditiis iure dolores cupiditate. Illum
-              dolorem atque sit, aperiam quis voluptatem ratione fugit?
-            </p>
+            <h2 className="text-xl font-bold">{speech}</h2>
+            <p className="text-sm opacity-75 mt-4">{description}</p>
           </div>
         </div>
       </div>
---
+++ data/props.ts
@@ -0,0 +1,13 @@
+
+export interface CatCardProps {
+    title: string;
+    pic: string;
+    speech: string;
+  }
+export interface ResCardProps {
+    title: string;
+    pic: string;
+    speech: string;
+    phonetic: string;
+    description: string;
+  }
