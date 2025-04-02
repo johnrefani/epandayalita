93b951eb54908e9ef3ef821b6b1a7a09c186f48f
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-04-02 22:12:09 +0800

    add function to search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import WordList from "@/components/WordList";
 import { Header, Main, About, Footer } from "@/lib/imports";
 
 export default function Home() {
   return (
     <main>
-
+      <Header />
       <div className="mx-[1rem] md:mx-[3.75rem] lg:mx-[6.25rem] xl:mx-[7.5rem]">
-        <WordList />
+       <Main />
+       <About />
       </div>
       <Footer />
     </main>
