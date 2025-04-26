a78f3857d75062ba1f4370c7f061662159aeaf80
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-04-27 01:28:53 +0800

    add infocard

--- app/tagapayo/page.tsx
+++ app/tagapayo/page.tsx
@@ -1,13 +1,29 @@
-import { Footer, Header } from '@/lib/imports'
-import React from 'react'
+import { Information } from "@/data";
+import { Footer, Header, InfoCard } from "@/lib/imports";
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <main>
-    <Header/>
-    <Footer/>
-    </main>
-  )
-}
+    <main className="overflow-hidden">
+      <Header />
+      <div className="flex-center py-9 bg-blue-800 -mx-[1rem] md:-mx-[3.75rem] lg:-mx-[6.25rem] xl:-mx-[7.5rem] mt-16 md:mt-20">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-blue-50 text-center">Impormasyon ng Tagapayo</h1>
+      </div>
 
-export default page
+      {Information.filter((person) => person.id === 3).map((person) => (
+        <div key={person.id} className="py-8 -mx-[1rem] md:mx-[3.75rem] lg:mx-[6.25rem] xl:mx-[7.5rem]">
+          <InfoCard
+            id={person.id}
+            name={person.name}
+            pic={person.pic}
+            description={person.description}
+          />
+        </div>
+      ))}
+
+      <Footer />
+    </main>
+  );
+};
+
+export default Page;
