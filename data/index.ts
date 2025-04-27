1a84df4b3af9f983b3cc7002c0d58910fb6990ed
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-04-27 15:04:32 +0800

    add data for sources

--- data/index.ts
+++ data/index.ts
@@ -109,3 +109,42 @@ export const Information = [
       "Tunay na isang edukador sa puso, siya ay mayroong malalim na pagmamahal sa ating sariling wika, ang Filipino. Ang kanyang paglalakbay sa edukasyon ay nagsimula sa Gaboc Elementary School sa magandang bayan ng Mercedes, Camarines Norte, at nagpatuloy sa Baesa Adventist Academy sa lunsod ng Caloocan. Tinapos niya ang kanyang Batsilyer ng Sekundaryang Edukasyon, nagpakadalubhasa sa Filipino, sa Naga View Adventist College sa masiglang lunsod ng Naga. Ang kanyang uhaw sa kaalaman ay nagtulak sa kanya upang ituloy ang Masterado sa Filipino sa Unibersidad ng Nueva Caceres, muli sa Naga City. At bilang sukdulan ng kanyang pormal na pag-aaral, nakamit niya ang Doktorado sa Pilosopiya sa Filipino mula sa prestihiyosong Bicol University sa Legazpi City. Bago pa man siya bumalik sa ating bayan, nagkaroon siya ng natatanging pagkakataong magbahagi ng kanyang galing sa internasyonal na entablado, naglilingkod bilang guro ng wika sa mga bansang Thailand, Cambodia, at Laos mula 2007 hanggang 2011. Mula 2012 hanggang 2017, ibinuhos niya ang kanyang dedikasyon sa pagtuturo sa DepEd Camarines Norte, humuhubog ng mga kabataan sa pampublikong paaralang sekundarya. Sa pagitan ng 2017 at 2019, siya ay naging mahalagang bahagi ng Central Bicol State University of Agriculture sa Pasacao, Camarines Sur, kung saan siya ay namuno bilang Language Cluster Head. Sa kasalukuyan, patuloy siyang naglilingkod sa Camarines Norte State College. Dito, kanyang hinahasa ang mga susunod na henerasyon ng mga guro sa pamamagitan ng pagtuturo sa Masterado sa Filipino sa graduate school, at nagbabahagi rin ng kanyang kaalaman sa College of Education.",
   },
 ];
+
+
+
+export const source = [
+  // PROSESO
+  { id: 1, name: "Parakaluban", category: "PROSESO", link: "https://youtu.be/XerFnYh2C74?si=Gbuz6HFnsxcp68_J" },
+  { id: 2, name: "Paratais", category: "PROSESO", link: "https://youtu.be/wK0jauE7i4Y?si=FH62M57uc1kmoFsA" },
+  { id: 3, name: "Paramaso", category: "PROSESO", link: "https://youtu.be/xr8TQfBC0EY?si=XCwh2HFUGAKtlkiw" },
+
+  // MGA PRODUKTO
+  { id: 4, name: "Asada", category: "MGA PRODUKTO", link: "https://sl.bing.net/fwnfHd2zXYO" },
+  { id: 5, name: "Samurai", category: "MGA PRODUKTO", link: "https://sl.bing.net/kyrvnDffFvM" },
+  { id: 6, name: "Karet", category: "MGA PRODUKTO", link: "https://images.app.goo.gl/QGdVZZ2YpirCH5sW9" },
+  { id: 7, name: "Palawan", category: "MGA PRODUKTO", link: "https://images.app.goo.gl/QuLWpWgKBU2R2CGZ9" },
+  { id: 8, name: "Cleaver", category: "MGA PRODUKTO", link: "https://sl.bing.net/kbVeGoCNFF6" },
+  { id: 9, name: "Suyod", category: "MGA PRODUKTO", link: "https://sl.bing.net/gDM4QE6W7e8" },
+  { id: 10, name: "Sinampalok", category: "MGA PRODUKTO", link: "https://www./pinoybladehunter/posts/2-sinampaloktop-/2105825606387289/" },
+  { id: 11, name: "Sungot-hipon", category: "MGA PRODUKTO", link: "https://images.app.goo.gl/eQZCxF22JwYy87iv8" },
+  { id: 12, name: "Ginunting", category: "MGA PRODUKTO", link: "https://www.traditionalfilipinoweapons.com/ginunting/" },
+  { id: 13, name: "Kaluban", category: "MGA PRODUKTO", link: "https://www.facebook.com/p/ItakKalubanPulo-made-to-order-100086190476525/" },
+  { id: 14, name: "Arado", category: "MGA PRODUKTO", link: "https://commons.wikimedia.org/wiki/File:Arado_secular.JPG" },
+  { id: 15, name: "Minasbad", category: "MGA PRODUKTO", link: "https://sl.bing.net/cnNoNUPnCbQ" },
+  { id: 16, name: "Palakol", category: "MGA PRODUKTO", link: "https://sl.bing.net/eAKu8ne2sOy" },
+
+  // MGA KAGAMITAN AT MATERYALES
+  { id: 17, name: "Kikil", category: "MGA KAGAMITAN AT MATERYALES", link: "https://images.app.goo.gl/SeqSWGxfQq89w9CE8" },
+  { id: 18, name: "Langis", category: "MGA KAGAMITAN AT MATERYALES", link: "https://sl.bing.net/gfZaJCKVhlY" },
+  { id: 19, name: "Blower", category: "MGA KAGAMITAN AT MATERYALES", link: "https://www.google.com/search?q=blower+=img#vhid=nU1zyQZIfB2V0M&/" },
+  { id: 20, name: "Hasaan", category: "MGA KAGAMITAN AT MATERYALES", link: "https://images.app.goo.gl/AZK8t4esuYVbkaGD8" },
+  { id: 21, name: "Pliers", category: "MGA KAGAMITAN AT MATERYALES", link: "https://images.pliersl/AZK8t4esuYVbkaGD8" },
+  { id: 22, name: "Anvil", category: "MGA KAGAMITAN AT MATERYALES", link: "https://images.anvil/f8LUA16nopQtgmHe8" },
+  { id: 23, name: "Varnish", category: "MGA KAGAMITAN AT MATERYALES", link: "https://images.app.goo.gl/qaJnTTby73EqDzEV6" },
+  { id: 24, name: "Grinder", category: "MGA KAGAMITAN AT MATERYALES", link: "https://sl.bing.net/eNTAq0zpXR6" },
+  { id: 25, name: "Kibal", category: "MGA KAGAMITAN AT MATERYALES", link: "https://sl.bing.net/kyCi6WKcmT6" },
+  { id: 26, name: "Lagari sa kahoy", category: "MGA KAGAMITAN AT MATERYALES", link: "https://sl.bing.net/fLi0WQqjBHE" },
+
+  // MGA BAHAGI
+  { id: 27, name: "Polo ng pala", category: "MGA BAHAGI", link: "https://sl.bing.net/gwYNcD4xzwG" },
+];
