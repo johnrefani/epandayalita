12eba894572c2ee45a9cf8ceb17b048b880f131a
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-04-27 16:04:15 +0800

    add source option in rescard

--- components/ui/ResCard.tsx
+++ components/ui/ResCard.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React from "react";
 import AudioPlayer from "./AudioPlayer";
 import { IoSearch } from "react-icons/io5";
+import { source } from "@/data";
+import Link from "next/link";
 
 const ResCard = ({
   id,
@@ -13,6 +15,10 @@ const ResCard = ({
   definition,
   pronunciation,
 }: ResCardProps) => {
+
+  const sourceItem = source.find(item => (item.id === id))
+
+
   return (
     <section className="relative flex justify-center pt-16 mt-16">
       <div className="absolute inset-0 grid grid-rows-[50%_50%]">
@@ -42,8 +48,9 @@ const ResCard = ({
           </div>
 
           <div className="flex-1 flex flex-col gap-4">
-            <div className="flex items-center gap-3 sm:gap-6">
-              <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-blue-950">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3 lg:gap-6">
+                <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-blue-950">
                 {word}
               </h1>
               {audio ? (
@@ -51,6 +58,29 @@ const ResCard = ({
               ) : (
                 <span className="text-sm md:text-base text-gray-500">No Audio</span>
               )}
+              </div>
+              {
+                sourceItem ? (
+                  <Link href={sourceItem.link} passHref>
+                    <div className="flex items-center justify-center gap-2 bg-blue-200 py-2 px-3 rounded-md">
+                      <p className="text-blue-950 text-xs md:text-sm lg:text-base hidden lg:block">
+                        Tignan ang pinagmulan
+                      </p>
+                      <Image 
+                        className="w-6 h-6 md:w-7 md:h-7 lg:w-8 lg:h-8"
+                        src="/Vector.svg"
+                        height={24}
+                        width={24}
+                        alt="source"
+                      />
+                    </div>
+                  </Link>
+                ) : (
+                  <>
+                  </>
+                )
+              }
+                 
             </div>
 
             <h2 className="w-fit text-sm sm:text-base lg:text-lg text-blue-700 bg-blue-100 rounded-md border border-blue-300 py-1 px-3 shadow-sm">
